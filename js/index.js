5c9da784ad764894b1690a358fb3abdf6fd5fb3d
sashka90694 <tepmeh90694@yandex.ru>
2018-03-20 22:42:29 +0300

    add adaptive menu, slider and features

--- js/index.js
+++ js/index.js
@@ -1,8 +1,14 @@
-// function myFunction(x) {
-// 	x.classList.toggle("change");
-// }
 $(document).ready(function (){
 	$('.mobile-ico').click(function(){
 		$(this).toggleClass('change');
-	})
-})
+		$('.header__menu menu').toggleClass('open');
+	});
+	$('.header__slider').slick({
+		dots: true,
+        infinite: true,
+        slidesToShow: 1,
+        autoplay: false,
+        arrows: false
+        // autoplaySpeed: 2000,
+	});
+});
